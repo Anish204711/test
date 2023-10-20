6a7d0daaec24b2d643bb42b173b9efa956e9b001
Anish204711 <anish204711@gmail.com>
2023-10-20 22:41:48 +0545

    ignored robrt.txt and adeed css extrator to spyder

--- news/news/spiders/newsspider.py
+++ news/news/spiders/newsspider.py
@@ -6,6 +6,8 @@ class NewsSpider(scrapy.Spider):
     start_urls=['https://thehimalayantimes.com/']
 
     def parse(self, response):
+        response.css('h3.alith_post_title a::text').extract()
+        response.css('li.pager-nav  a::attr("href")').extract()
         list_news_types=['Economics','Politics','Crime','Sports','International','National']
         title=response.css('title::text').extract()
         yield {'titletext':title}
