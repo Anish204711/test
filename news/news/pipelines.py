9b364c65cea8339015d65aa1e8322274fa3b0a8f
Anish204711 <anish204711@gmail.com>
2024-02-03 15:31:01 +0545

    Your commit message

--- news/news/pipelines.py
+++ news/news/pipelines.py
@@ -6,8 +6,30 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class NewsPipeline:
+    def __init__(self) -> None:
+        self.create_conn()
+        self.create_table()
+
+    def create_conn(self):
+        database_path = 'my_database.db'
+        # Create a connection to the SQLite database
+        self.connection = sqlite3.connect(database_path)
+        # Create a cursor object using the connection
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS news_table
+                  ( date TEXT, author TEXT,title TEXT,content TEXT)''')
+
+
     def process_item(self, item, spider):
+        data_to_insert=(item["date"],item["author"],item["title"],item["content"])
+        self.store_data(self,data_to_insert)
         return item
+    
+    def store_data(self,data_to_insert):
+        self.cursor.executemany('INSERT INTO news_table (date, author, title,content) VALUES (?, ?, ?,?)', data_to_insert)
+        self.connection.commit()
--- news/news/spiders/newsspider.py
+++ news/news/spiders/newsspider.py
@@ -1,15 +1,42 @@
 import scrapy
-
+from ..items import NewsItem
 class NewsSpider(scrapy.Spider):
     name='newsspider'
     allowed_domain=['thehimalayantimes.com']
-    start_urls=['https://thehimalayantimes.com/']
+    start_urls=['https://thehimalayantimes.com/search?query=politics',
+                'https://thehimalayantimes.com/search?query=economics',
+                'https://thehimalayantimes.com/search?query=internationals',
+                'https://thehimalayantimes.com/search?query=sports',
+                ]
+    
 
     def parse(self, response):
-        response.css('h3.alith_post_title a::text').extract()
-        response.css('li.pager-nav  a::attr("href")').extract()
-        list_news_types=['Economics','Politics','Crime','Sports','International','National']
-        title=response.css('title::text').extract()
-        yield {'titletext':title}
+        
+        # list of all available news links in a page.
+        news_detail_links=response.css(".animated-fast a::attr('href')").extract()
+        for link in news_detail_links:
+            yield response.follow(link, callback=self.parse_link)
+        # i=0
+    
+        #next_page=new_link+'&pgno='+str(i+1)
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self.parse)
+
+    def parse_link(self, response):
+        items=NewsItem()
+        news_title=response.css('h1.alith_post_title::text').extract()
+        news_author=response.css('div.article_author_name::text').extract()
+        news_date=response.css('div.article_date::text').extract() 
+        news_content=response.css('article div.post-content div.single-content div.animate-box').extract()
+       
+        # Adding data to items
+        items['title']=news_title
+        items['author']=news_author
+        items['date']=news_date
+        items['content']=news_content
+        return items
+        
+        
+                
         
 
